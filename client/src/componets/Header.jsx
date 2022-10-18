e79d34d75c83cd7db9052c1a7f73e541dc8845f4
Everett Cooley <cooleyeverett4@gmail.com>
2022-10-17 17:36:51 -0700

    Add credit in header

--- client/src/componets/Header.jsx
+++ client/src/componets/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Header() {
-  return (
-    <div className='header-container'>
-        <h1 className='cut-text header'>Snowboards R US</h1>
-    </div>
-  )
+    return (
+        <>
+            <div className='watermark'>Created by Everett Cooley</div>
+            <div className='header-container'>
+                <h1 className='cut-text header'>Snowboards R US</h1>
+            </div>
+        </>
+
+    )
 }
